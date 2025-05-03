4068042d9910dc37285957fedbc503422b1ea5b0
st_offer <bwg.it2682@gmail.com>
2025-05-03 18:30:18 +0300

    upd learning service

---
+++ learning/pkg/api/grpc/conn.go
@@ -0,0 +1,55 @@
+package grpc
+
+import (
+	"context"
+
+	v1 "github.com/glamostoffer/arete/learning/pkg/api/grpc/v1"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type connector struct {
+	v1.LearningClient
+
+	cfg  Config
+	conn *grpc.ClientConn
+}
+
+const (
+	componentName = "grpc-connector"
+)
+
+func New(cfg Config) *connector {
+	return &connector{
+		cfg: cfg,
+	}
+}
+
+func (c *connector) Start(ctx context.Context) error {
+	options := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+	}
+
+	conn, err := grpc.NewClient(
+		c.cfg.Address,
+		options...,
+	)
+	if err != nil {
+		return err
+	}
+
+	c.conn = conn
+	c.LearningClient = v1.NewLearningClient(c.conn)
+
+	return nil
+}
+
+func (c *connector) Stop(ctx context.Context) error {
+	return c.conn.Close()
+}
+
+func (c *connector) GetName() string {
+	return componentName
+}
--- learning/pkg/go.mod
+++ learning/pkg/go.mod
@@ -5,11 +5,18 @@ go 1.24.0
 replace github.com/glamostoffer/arete/learning => ../
 
 require (
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.60.0
 	google.golang.org/grpc v1.72.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
